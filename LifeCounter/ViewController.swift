f5a8b45de5623f9fb27b4f2cf53758e9f31a27e1
Christina Wang <christinawang6000@gmail.com>
2026-01-31 23:14:29 -0800

    added code

--- LifeCounter/ViewController.swift
+++ LifeCounter/ViewController.swift
@@ -10,22 +10,64 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var playersStack: UIStackView!
+    @IBOutlet weak var player1LifeLabel: UILabel!
+    @IBOutlet weak var player2LifeLabel: UILabel!
+    @IBOutlet weak var loserLabel: UILabel!
+
+    private var p1Life = 20
+    private var p2Life = 20
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        loserLabel.text = ""
+        updateLifeLabels()
+        updateLoserLabel()
+
+        // Set initial layout direction
+        playersStack.axis = view.bounds.width > view.bounds.height ? .horizontal : .vertical
     }
 
-    override func viewWillTransition(
-        to size: CGSize,
-        with coordinator: UIViewControllerTransitionCoordinator
-    ) {
-        super.viewWillTransition(to: size, with: coordinator)
+    private func updateLifeLabels() {
+        player1LifeLabel.text = "\(p1Life)"
+        player2LifeLabel.text = "\(p2Life)"
+    }
 
+    private func updateLoserLabel() {
+        if p1Life <= 0 {
+            loserLabel.text = "Player 1 LOSES!"
+        } else if p2Life <= 0 {
+            loserLabel.text = "Player 2 LOSES!"
+        } else {
+            loserLabel.text = ""
+        }
+    }
+
+    private func changeLife(player: Int, delta: Int) {
+        if player == 1 {
+            p1Life += delta
+        } else {
+            p2Life += delta
+        }
+
+        updateLifeLabels()
+        updateLoserLabel()
+    }
+
+    @IBAction func p1Plus(_ sender: UIButton) { changeLife(player: 1, delta: 1) }
+    @IBAction func p1Minus(_ sender: UIButton) { changeLife(player: 1, delta: -1) }
+    @IBAction func p1Plus5(_ sender: UIButton) { changeLife(player: 1, delta: 5) }
+    @IBAction func p1Minus5(_ sender: UIButton) { changeLife(player: 1, delta: -5) }
+
+    @IBAction func p2Plus(_ sender: UIButton) { changeLife(player: 2, delta: 1) }
+    @IBAction func p2Minus(_ sender: UIButton) { changeLife(player: 2, delta: -1) }
+    @IBAction func p2Plus5(_ sender: UIButton) { changeLife(player: 2, delta: 5) }
+    @IBAction func p2Minus5(_ sender: UIButton) { changeLife(player: 2, delta: -5) }
+
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
         coordinator.animate(alongsideTransition: { _ in
-            self.playersStack.axis =
-                size.width > size.height ? .horizontal : .vertical
+            self.playersStack.axis = size.width > size.height ? .horizontal : .vertical
         })
     }
 }
-
-
