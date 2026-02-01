532f98c98500a7f94533178c8268dd30ba0965a9
Christina Wang <christinawang6000@gmail.com>
2026-01-31 18:08:41 -0800

    Initial Commit

---
+++ LifeCounter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LifeCounter
+//
+//  Created by Christina Wang on 1/31/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
